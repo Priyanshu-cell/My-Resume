6b1b3ae5016c62c50973b911417d042f75b1d547
Priyanshu Maithani <priyanshumaithani02@gmail.com>
2024-02-25 20:48:40 +0530

    smooth scroll and skill section bar filling animation added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
 var interval;
-
 /*
+
 for (var i = 0; i < navMenuAnchorTags.length; i++) {
     navMenuAnchorTags[i].addEventListener('click', function (event) {
         event.preventDefault();
@@ -27,3 +27,82 @@ function scrollVertically(targetSection) {
     window.scrollBy(0, 50);
 }
 */
+//     smooth scroll
+
+// 1. first line select all anchor tags whose href link starts with # example- #portfolio
+// The ^= in the attribute selector means "starts with".
+
+//2. 
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
+
+
+/*
+
+                        /*skill bar animation  */
+
+// handle scroll event
+var progressBars= document.querySelectorAll(".skills-progress > div");
+var skillContainer = document.getElementById("skills-container");
+addEventListener("scroll", checkScroll);
+
+//when we scroll bar so the must me zero
+
+function initialiseBar(bar) {
+    bar.setAttribute("data-visited", false);
+    bar.style.width = 0 + '%';
+}
+
+for (var bar of progressBars) {
+    initialiseBar(bar);
+}
+function fillBar(bar) {
+
+    var currentWidth = 0;
+    var targetWidth = bar.getAttribute("data-bar-width");
+    var interval = setInterval(function () {
+        if (currentWidth >= targetWidth) {
+            clearInterval(interval);
+            return;
+        }
+        currentWidth++;
+        bar.style.width = currentWidth + '%';
+    }, 5);
+
+}
+
+// This function uses a for loop for individual progress bars.
+
+function checkScroll() {
+
+    for (let bar of progressBars) {
+        var barCoordinates = bar.getBoundingClientRect();
+        if ((bar.getAttribute("data-visited") == "false") &&
+            (barCoordinates.top <= (window.innerHeight - barCoordinates.height))) {
+            bar.setAttribute("data-visited", true);
+            fillBar(bar);
+        } else if (barCoordinates.top > window.innerHeight) {
+            bar.setAttribute("data-visited", false);
+            initialiseBar(bar);
+        }
+
+    }
+}
+
+
+
+window.addEventListener("scroll", checkScroll);
+
+// This event fills the progress bars if they are displayed on the screen when the page is loaded.
+//window.addEventListener("load", checkScroll);
